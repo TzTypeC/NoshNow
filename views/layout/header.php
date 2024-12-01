70f1c4efc51a28f6123d381dd500a611f11c7595
jekthw <mzakky.wijaya@gmail.com>
2024-12-01 16:05:32 +0700

    Navbar 'a' href tweak, Fixed navbar for views/profile/ page

--- views/layout/header.php
+++ views/layout/header.php
@@ -16,10 +16,10 @@
             <!-- Left Buttons Wrapper -->
             <div class="flex items-center gap-10">
                 <!-- NoshNow Title-->
-                <div class="flex items-center gap-3">
+                <a href="../landing/" class="flex items-center gap-3">
                     <i class='text-4xl text-center bx bxs-bowl-rice text-primary-500'></i>
                     <h1 class="font-semibold text-[2.5rem] text-primary-500">NoshNow</h1>
-                </div>
+                </a>
                 <!-- Buttons -->
                 <ul class="flex items-center h-full font-medium *:*:px-4  *:cursor-pointer *:*:py-4 *:align-middle ">
                     <!-- <?php var_dump($url)?> -->
@@ -36,6 +36,13 @@
                     </li>
                     <li>
                         <a class="border-b-4 border-primary-500 text-primary-500">Kantin</a>
+                    </li>
+                    <?php elseif($url=='/NoshNow/views/profile/' || $url=='/NoshNow/views/kantin/profile.php') : ?>
+                    <li>
+                        <a class="text-gray-500 hover:text-black" href="../landing">Home</a>
+                    </li>
+                    <li>
+                        <a class="text-gray-500 hover:text-black" href="../kantin">Kantin</a>
                     </li>
                     <?php endif; ?>
                 </ul>
